b0d7e543b52be331e4219c43cb380c4079648739
Erwan <e-roussel@outlook.com>
2020-05-18 16:29:26 +0200

    Add ssh key generation

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,3 +5,13 @@ sudo apt install git
 # Config git
 git config --global user.email "e-roussel@outlook.com"
 git config --global user.name "Erwan ROUSSEL"
+
+# Add ssh key
+ssh-keygen -t rsa -b 4096 -C "e-roussel@outlook.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+
+sudo apt-get install xclip
+xclip -sel clip < ~/.ssh/id_rsa.pub
+
+echo "Done! SSH key copied to clipboard"
