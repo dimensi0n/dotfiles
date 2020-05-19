10d41ac095e316834dd91d6ce681c6c5202a6804
Erwan <e-roussel@outlook.com>
2020-05-19 10:33:02 +0200

    Add i3-gaps installation

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -2,6 +2,10 @@
 # Install i3
 sudo apt install i3
 
+# Install i3-gaps
+sudo add-apt-repository ppa:kgilmer/speed-ricer
+sudo apt install i3-gaps
+
 # Copy config
 mkdir ~/.config && mkdir ~/.config/i3
 cp config/i3/config ~/.config/i3
