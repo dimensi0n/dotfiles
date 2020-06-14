68e8a4363be48a9f8b1e6c77d185ef1ca4aac1a7
Erwan <e-roussel@outlook.com>
2020-06-14 14:09:27 +0200

    add custom fonts to script

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -3,4 +3,5 @@
 git clone https://github.com/gabrielelana/awesome-terminal-fonts.git
 
 cp awesome-terminal-fonts/build/* ~/.fonts
+cp fonts/truetype/* /usr/share/fonts/truetype
 fc-cache -fv ~/.fonts
